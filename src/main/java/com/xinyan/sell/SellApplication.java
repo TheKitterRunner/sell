a41b73e53997fabd6bf1f88e0a0f710971f69455
Nico <306106354@qq.com>
2018-11-15 20:58:39 +0800

    增加商品列表....

--- src/main/java/com/xinyan/sell/SellApplication.java
+++ src/main/java/com/xinyan/sell/SellApplication.java
@@ -3,6 +3,8 @@ package com.xinyan.sell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @SpringBootApplication
 public class SellApplication {
 
